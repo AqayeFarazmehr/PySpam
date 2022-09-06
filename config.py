cdd3bb40ebe2624f2938ffc19f4c53aa7df819be
SlavPH <110917771+SlavPH@users.noreply.github.com>
2022-09-06 09:41:15 -0400

    New Headers
    + Logo changed
    + Headers addded

--- config.py
+++ config.py
@@ -19,12 +19,12 @@ flash='\033[5m'         # Flash
 # Logo
 logo = (f"""
 
-{flash}{white}███████╗██████╗  █████╗ ███╗   ███╗  {red}  ██████╗ ██╗   ██╗
-{flash}{white}██╔════╝██╔══██╗██╔══██╗████╗ ████║  {red}  ██╔══██╗╚██╗ ██╔╝
-{flash}{white}███████╗██████╔╝███████║██╔████╔██║  {red}  ██████╔╝ ╚████╔╝ 
-{flash}{white}╚════██║██╔═══╝ ██╔══██║██║╚██╔╝██║  {red}  ██╔═══╝   ╚██╔╝  
-{flash}{white}███████║██║     ██║  ██║██║ ╚═╝ ██║  {red}  ██║        ██║   
-{flash}{white}╚══════╝╚═╝     ╚═╝  ╚═╝╚═╝     ╚═╝  {red}  ╚═╝        ╚═╝   
+{flash}{green}███████╗██████╗  █████╗ ███╗   ███╗  {blue}  ██████╗ ██╗   ██╗
+{flash}{green}██╔════╝██╔══██╗██╔══██╗████╗ ████║  {blue}  ██╔══██╗╚██╗ ██╔╝
+{flash}{white}███████╗██████╔╝███████║██╔████╔██║  {blue}  ██████╔╝ ╚████╔╝ 
+{flash}{white}╚════██║██╔═══╝ ██╔══██║██║╚██╔╝██║  {blue}  ██╔═══╝   ╚██╔╝  
+{flash}{red}███████║██║     ██║  ██║██║ ╚═╝ ██║  {blue}  ██║        ██║   
+{flash}{red}╚══════╝╚═╝     ╚═╝  ╚═╝╚═╝     ╚═╝  {blue}  ╚═╝        ╚═╝   
 {reset}                                                        
 """)
 
@@ -141,4 +141,188 @@ BamaHeader = {
     "sec-fetch-mode": "cors",
     "sec-fetch-site": "same-origin",
     "x-requested-with": "XMLHttpRequest"
+}
+
+DigiKalaHeader = {
+    "Host": "api.digikala.com",
+    "User-Agent": "".join(random.choices(user_agents)),
+    "Accept": "application/json, text/plain, */*",
+    "Accept-Language": "en-US,en;q=0.5",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Content-Type": "application/json",
+    "X-Web-Optimize-Response": "1",
+    "X-Web-Client": "desktop",
+    "Content-Length": "57",
+    "Origin": "https://www.digikala.com",
+    "Connection": "keep-alive",
+    "Referer": "https://www.digikala.com/",
+    "Cookie": "tracker_glob_new=68O3R39; tracker_session=bQEHxR4; TS01e4b47a=0102310591e6514d31309b081241eb51abd1ca76f2a7f048e58f2f0b0b14dbe94b741af431c6b5faf9c1ef2eb9138be99665e63214231507450cfa92ca173360e507a9b90380d0c1d5b585f0c4f357e245eadd8edf; _sp_ses.13cb=*; _sp_id.13cb=78186e05-414b-4b29-a434-f5ae196220b0.1662461959.1.1662461974.1662461959.6ab98e45-8f5d-4c09-915f-79efd3b6f0da",
+    "Sec-Fetch-Dest": "empty",
+    "Sec-Fetch-Mode": "cors",
+    "Sec-Fetch-Site": "same-site",
+    "DNT": "1",
+    "Sec-GPC": "1",
+    "TE": "trailers"
+    }
+
+AlibabaHeader = {
+    "Host": "ws.alibaba.ir",
+    "User-Agent": "".join(random.choices(user_agents)),
+    "Accept": "application/json, text/plain, */*",
+    "Accept-Language": "en-US,en;q=0.5",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Content-Type": "application/json",
+    "ab-channel": "WEB-NEW,PRODUCTION,CSR,www.alibaba.ir,N,Firefox,91.0,N,N,Linux",
+    "Content-Length": "28",
+    "Origin": "https://www.alibaba.ir",
+    "Connection": "keep-alive",
+    "Referer": "https://www.alibaba.ir/",
+    "Sec-Fetch-Dest": "empty",
+    "Sec-Fetch-Mode": "cors",
+    "Sec-Fetch-Site": "same-site",
+    "DNT": "1",
+    "Sec-GPC": "1"
+    }
+
+SheypoorHeader = {
+    "Host": "www.sheypoor.com",
+    "User-Agent": "".join(random.choices(user_agents)),
+    "Accept": "application/json",
+    "Accept-Language": "en-US,en;q=0.5",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Content-Type": "application/json;charset=utf-8",
+    "Content-Length": "26",
+    "Origin": "https://www.sheypoor.com",
+    "Connection": "keep-alive",
+    "Referer": "https://www.sheypoor.com/session",
+    "Cookie": "AMP_TOKEN=%24ERROR; ts=f1b5d1185a29489786bbb32e0e4590a1; track_id=64e2daddd086c1d0d0351987c65df966; _ga=GA1.2.502817657.1662464768; _gid=GA1.2.1845818780.1662464768; _gat=1",
+    "Sec-Fetch-Dest": "empty",
+    "Sec-Fetch-Mode": "cors",
+    "Sec-Fetch-Site": "same-origin",
+    "DNT": "1",
+    "Sec-GPC": "1",
+    "TE": "trailers"
+    }
+
+KomodaHeader = {
+    "Host": "api.komodaa.com",
+    "User-Agent": "".join(random.choices(user_agents)),
+    "Accept": "application/json",
+    "Accept-Language": "en-US,en;q=0.5",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Content-Type": "application/json",
+    "web-user-agent": "komodaa/5.8.6.236 " + "".join(random.choices(user_agents)),
+    "install-ref": "WEB",
+    "Content-Length": "30",
+    "Origin": "https://www.komodaa.com",
+    "Connection": "keep-alive",
+    "Referer": "https://www.komodaa.com/",
+    "Sec-Fetch-Dest": "empty",
+    "Sec-Fetch-Mode": "cors",
+    "Sec-Fetch-Site": "same-site",
+    "DNT": "1",
+    "Sec-GPC": "1"
+}
+
+BornosHeader = {
+    "Host": "bornosmode.com",
+    "User-Agent": "".join(random.choices(user_agents)),
+    "Accept": "*/*",
+    "Accept-Language": "en-US,en;q=0.5",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+    "X-CSRF-TOKEN": "mNvstxgBZVkBJVQpq1rl36iH3AJ2h0jBRzgB18ZZ",
+    "X-Requested-With": "XMLHttpRequest",
+    "Content-Length": "28",
+    "Origin": "https://bornosmode.com",
+    "Connection": "keep-alive",
+    "Referer": "https://bornosmode.com/login/?_back=https%3A%2F%2Fbornosmode.com%2F",
+    "Cookie": "XSRF-TOKEN=eyJpdiI6Im80SzdBZ2N6UEZHTGcxeTVwYXN3WlE9PSIsInZhbHVlIjoiYUcvdWxkWC9zd05Fbk5sZjJEOVprS0cyYSszWVNjVEtMMlJCbGNSWjlPY08vRlF5SlZDWkZHb2hPMHlTWklucFFyK0lTL3FIWnYrb2FVSUZUK1RLSEVMWlMwL0I5VHNpOUU3ZjM2a25Ublozd05LZmFIeFRjVzRqTGZ3R0w1T1QiLCJtYWMiOiJmNGRkOTY4ZWJjM2ExZGI1NDAxY2UzZmU0MDlkZjE5NzRiYTNlNDcyMTE5MTRhYTEyYWNmZjFiZDQwMDIxYTY4IiwidGFnIjoiIn0%3D; bornosmode_session=eyJpdiI6IllIR2pzcG1NRUwvbDNOMHhTNlgvQnc9PSIsInZhbHVlIjoiUkt0MzdkcEtFbExWL25IZVZZd2xBQkJuazdwV3BGMkorbkYyQ3JxYVM4andoZERWeTBEV1c2U1FWaE1OLzduV1owN3lCVzh2bUZpQjlxWXNJb0NXRGZTZ1pqakFNZlJHUE9QaWtZNTlEYzFBeXpUSUJmTnNNUHBlaXkrWkhhdFQiLCJtYWMiOiI3OGYyN2QxNWExNTFiMjRmZDRhNjcwNjU2YzQzNTg5Y2E0OWYzMzE2ZGVmMjYxNzYyYTdiOTBjZTY3YTBmNWFhIiwidGFnIjoiIn0%3D",
+    "Sec-Fetch-Dest": "empty",
+    "Sec-Fetch-Mode": "cors",
+    "Sec-Fetch-Site": "same-origin",
+    "DNT": "1",
+    "Sec-GPC": "1",
+    "TE": "trailers"
+}
+
+BaarbaanetHeader = {
+    "Host": "baarbaanet.com",
+    "User-Agent": "".join(random.choices(user_agents)),
+    "Accept": "application/json, text/plain, */*",
+    "Accept-Language": "en-US,en;q=0.5",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Content-Type": "application/json;charset=utf-8",
+    "CSN": "395733078",
+    "CVS": "2.0.0",
+    "CTY": "W",
+    "Content-Length": "55",
+    "Origin": "https://baarbaanet.com",
+    "Connection": "keep-alive",
+    "Referer": "https://baarbaanet.com/login",
+    "Cookie": "wfpk=395733078",
+    "Sec-Fetch-Dest": "empty",
+    "Sec-Fetch-Mode": "cors",
+    "Sec-Fetch-Site": "same-origin",
+    "DNT": "1",
+    "Sec-GPC": "1",
+    "TE": "trailers"
+}
+
+LivitoHeader = {
+    "Host": "livito.tv",
+    "User-Agent": "".join(random.choices(user_agents)),
+    "Accept": "application/json, text/plain, */*",
+    "Accept-Language": "en-US,en;q=0.5",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Content-Type": "application/json",
+    "Content-Length": "83",
+    "Origin": "https://livito.tv",
+    "Connection": "keep-alive",
+    "Referer": "https://livito.tv/",
+    "Sec-Fetch-Dest": "empty",
+    "Sec-Fetch-Mode": "cors",
+    "Sec-Fetch-Site": "same-origin",
+    "DNT": "1",
+    "Sec-GPC": "1",
+    "TE": "trailers"
+}
+
+ZarinplusHeader = {
+    "Host": "api.zarinplus.com",
+    "User-Agent": "".join(random.choices(user_agents)),
+    "Accept": "*/*",
+    "Accept-Language": "en-US,en;q=0.5",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Content-Type": "application/json; charset=utf-8",
+    "Content-Length": "31",
+    "Origin": "https://pwa.zarinplus.com",
+    "Sec-Fetch-Dest": "empty",
+    "Sec-Fetch-Mode": "cors",
+    "Sec-Fetch-Site": "same-site",
+    "DNT": "1",
+    "Sec-GPC": "1",
+    "Referer": "https://pwa.zarinplus.com/",
+    "Connection": "keep-alive",
+    "TE": "trailers"
+}
+
+BehtarinoHeader = {
+    "Host": "bck.behtarino.com",
+    "User-Agent": "".join(random.choices(user_agents)),
+    "Accept": "application/json",
+    "Accept-Language": "en-US,en;q=0.5",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Content-Type": "application/json",
+    "User-Id": "HujvHsu79nHn3DnAXStfHnTU6dniYk",
+    "Content-Length": "23",
+    "Origin": "https://behtarino.com",
+    "Connection": "keep-alive",
+    "Referer": "https://behtarino.com/",
+    "Sec-Fetch-Dest": "empty",
+    "Sec-Fetch-Mode": "cors",
+    "Sec-Fetch-Site": "same-site",
+    "DNT": "1",
+    "Sec-GPC": "1",
+    "TE": "trailers"
 }
